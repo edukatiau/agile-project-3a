9270ddc379a3e665ef823809159a9faddbc70c75
Eduardo Rigon <edukatiau@gmail.com>
2024-07-27 23:42:24 -0300

    Teste

--- src/main/java/ifsul/agileproject/rachadinha/RachadinhaApplication.java
+++ src/main/java/ifsul/agileproject/rachadinha/RachadinhaApplication.java
@@ -2,14 +2,22 @@ package ifsul.agileproject.rachadinha;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @SpringBootApplication
+@Controller
 public class RachadinhaApplication {
 
   public static void main(String[] args) {
     SpringApplication.run(RachadinhaApplication.class, args);
     System.out.println("Hello, world!");
     System.out.println("Just a Test!");
+  }
+
+  @GetMapping("/")
+  public String welcome(){
+    return "Hello World";
   }
 
 }
