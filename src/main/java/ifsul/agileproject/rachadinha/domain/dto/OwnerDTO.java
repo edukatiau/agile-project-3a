9ed67a8aa1f96f5e3fb504b2cb3adea2543594ff
Eduardo Rigon <edukatiau@gmail.com>
2024-07-27 21:45:14 -0300

    Correção

--- src/main/java/ifsul/agileproject/rachadinha/domain/dto/OwnerDTO.java
+++ src/main/java/ifsul/agileproject/rachadinha/domain/dto/OwnerDTO.java
@@ -1,8 +1,16 @@
 package ifsul.agileproject.rachadinha.domain.dto;
 
 import ifsul.agileproject.rachadinha.domain.entity.User;
+import lombok.AllArgsConstructor;
+import lombok.Data;
 
-public record OwnerDTO(long id, String name, String email) {
+@AllArgsConstructor
+@Data
+public class OwnerDTO {
+
+  private Long id;
+  private String name;
+  private String email;
 
   public static OwnerDTO transformaEmDTO(User user){
     return new OwnerDTO(user.getId(), user.getName(), user.getEmail());
