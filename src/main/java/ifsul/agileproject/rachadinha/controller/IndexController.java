7702b3aeb4fdcf9815dc6208986cd1b005edbe4f
Eduardo Rigon <edukatiau@gmail.com>
2024-07-28 13:53:25 -0300

    Configurando

---
+++ src/main/java/ifsul/agileproject/rachadinha/controller/IndexController.java
@@ -0,0 +1,14 @@
+package ifsul.agileproject.rachadinha.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class IndexController {
+
+  @RequestMapping("/")
+  public String indexRachadinha(){
+    return "indexRachadinha";
+  }
+
+}
