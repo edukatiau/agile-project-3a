2b01e70b4f728c3c62a02e7776b7c0b652ac67c0
Eduardo Rigon <edukatiau@gmail.com>
2024-08-08 17:30:22 -0300

    Fix: bug heroku push

--- src/main/java/ifsul/agileproject/rachadinha/domain/dto/OwnerDTO.java
+++ src/main/java/ifsul/agileproject/rachadinha/domain/dto/OwnerDTO.java
@@ -1,16 +1,8 @@
 package ifsul.agileproject.rachadinha.domain.dto;
 
 import ifsul.agileproject.rachadinha.domain.entity.User;
-import lombok.AllArgsConstructor;
-import lombok.Data;
 
-@AllArgsConstructor
-@Data
-public class OwnerDTO {
-
-  private Long id;
-  private String name;
-  private String email;
+public record OwnerDTO(long id, String name, String email) {
 
   public static OwnerDTO transformaEmDTO(User user){
     return new OwnerDTO(user.getId(), user.getName(), user.getEmail());
